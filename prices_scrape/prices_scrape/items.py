40141548f8489b73ac09b85b8890d3d61d055757
Anton Shyshko <c-ashyshko@contentanalyticsinc.com>
2021-04-14 17:16:23 +0300

    complete test work

--- prices_scrape/prices_scrape/items.py
+++ prices_scrape/prices_scrape/items.py
@@ -7,6 +7,5 @@ import scrapy
 
 
 class PricesScrapeItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    price = scrapy.Field()
+    currency = scrapy.Field()
--- prices_scrape/prices_scrape/spiders/hellogetsafe.py
+++ prices_scrape/prices_scrape/spiders/hellogetsafe.py
@@ -1,23 +1,42 @@
-import jmespath
 import json
+import jmespath
+from datetime import date
 
 import scrapy
 from scrapy import Request
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst
+
+from prices_scrape.items import PricesScrapeItem
+
+
+class HellogetsafeLoader(ItemLoader):
+    default_output_processor = TakeFirst()
 
 
 class HellogetsafeSpider(scrapy.Spider):
     name = 'hellogetsafe'
     allowed_domains = ['hellogetsafe.com',
                        'www.hellogetsafe.com',
-                       'https://insurance-service.api.getsafe.eu']
+                       'insurance-service.api.getsafe.eu']
 
     DEFAULT_ZIP_CODE = '01067'
     DEFAULT_FAMILY_COVERAGE = False
+    DEFAULT_DRONE_COVERAGE = False
+    PAYLOAD = {}
 
     def __init__(self, *a, **kw):
         super(HellogetsafeSpider, self).__init__(*a, **kw)
+        true_values = ('1', 1, True, 'true', 'True')
+
         self.zip_code = kw.get('zip_code', self.DEFAULT_ZIP_CODE)
+
         self.family_coverage = kw.get('family_coverage', self.DEFAULT_FAMILY_COVERAGE)
+        self.family_coverage = self.family_coverage in true_values
+
+        self.drone_coverage = kw.get('drone_coverage', self.DEFAULT_DRONE_COVERAGE)
+        self.drone_coverage = self.drone_coverage in true_values
+
         self.PAYLOAD = {
             "product_configurations": [
                 {
@@ -28,10 +47,22 @@ class HellogetsafeSpider(scrapy.Spider):
                         "zip_code": self.zip_code,
                         "number_of_claims_in_last_5y": 0
                     },
-                    "effective_on": "2021-04-14"
+                    "effective_on": str(date.today())
+                },
+                {
+                    "product_key": "liability_premium_drones_neodigital_de",
+                    "configuration_data": {
+                        "deductible_amount": 0,
+                        "family_coverage": self.family_coverage,
+                        "zip_code": self.zip_code,
+                        "number_of_claims_in_last_5y": 0
+                    },
+                    "effective_on": str(date.today())
                 }
             ]
         }
+        if not self.drone_coverage:
+            del self.PAYLOAD.get('product_configurations')[1]
 
     def start_requests(self):
         yield Request(
@@ -42,4 +73,18 @@ class HellogetsafeSpider(scrapy.Spider):
         )
 
     def parse(self, response, **kwargs):
-        pass
+        item = PricesScrapeItem()
+        data = json.loads(response.body)
+        loader = HellogetsafeLoader(item=item, response=response)
+
+        loader.add_value('price', self._parse_price(data))
+        loader.add_value('currency', 'EUR')
+
+        loader.load_item()
+        yield item
+
+    @staticmethod
+    def _parse_price(data):
+        j = 'product_configurations[].price.gross_premium'
+        prices = jmespath.search(j, data)
+        return sum(map(float, filter(None, prices))) if prices else None
