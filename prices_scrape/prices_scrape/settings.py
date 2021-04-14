c0235b79abdbbf24cb436034f544ce4553f34bf8
Anton Shyshko <c-ashyshko@contentanalyticsinc.com>
2021-04-14 13:09:06 +0300

    working draft

--- prices_scrape/prices_scrape/settings.py
+++ prices_scrape/prices_scrape/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'prices_scrape.spiders'
 #USER_AGENT = 'prices_scrape (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- prices_scrape/prices_scrape/spiders/hellogetsafe.py
+++ prices_scrape/prices_scrape/spiders/hellogetsafe.py
@@ -1,10 +1,45 @@
+import jmespath
+import json
+
 import scrapy
+from scrapy import Request
 
 
 class HellogetsafeSpider(scrapy.Spider):
     name = 'hellogetsafe'
-    allowed_domains = ['hellogetsafe.com', 'www.hellogetsafe.com']
-    start_urls = ['https://www.hellogetsafe.com/']
+    allowed_domains = ['hellogetsafe.com',
+                       'www.hellogetsafe.com',
+                       'https://insurance-service.api.getsafe.eu']
 
-    def parse(self, response):
+    DEFAULT_ZIP_CODE = '01067'
+    DEFAULT_FAMILY_COVERAGE = False
+
+    def __init__(self, *a, **kw):
+        super(HellogetsafeSpider, self).__init__(*a, **kw)
+        self.zip_code = kw.get('zip_code', self.DEFAULT_ZIP_CODE)
+        self.family_coverage = kw.get('family_coverage', self.DEFAULT_FAMILY_COVERAGE)
+        self.PAYLOAD = {
+            "product_configurations": [
+                {
+                    "product_key": "liability_premium_basic_neodigital_de",
+                    "configuration_data": {
+                        "deductible_amount": 0,
+                        "family_coverage": self.family_coverage,
+                        "zip_code": self.zip_code,
+                        "number_of_claims_in_last_5y": 0
+                    },
+                    "effective_on": "2021-04-14"
+                }
+            ]
+        }
+
+    def start_requests(self):
+        yield Request(
+            url='https://insurance-service.api.getsafe.eu/api/v2/markets/de/prices',
+            method='POST',
+            body=json.dumps(self.PAYLOAD),
+            headers={'Content-Type': 'application/json;charset=UTF-8'},
+        )
+
+    def parse(self, response, **kwargs):
         pass
